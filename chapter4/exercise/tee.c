25fa15b684f1f5e01c5637c10c0371256fe36c6e
mehophd <ivivmih@gmail.com>
2026-08-21 18:10:39 +0300

    fix

--- chapter4/exercise/tee.c
+++ chapter4/exercise/tee.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[]) {
     ssize_t numRead;
     int outputFd, openFlags;
     mode_t filePerms;
+    char *filename;
 
     if (argc < 2 || argc > 3 || strcmp(argv[1], "--help") == 0)
         usageErr("%s [-a] file\n", argv[0]);
@@ -22,20 +23,31 @@ int main(int argc, char *argv[]) {
     if (strcmp("-a", argv[1]) == 0) {
         if (argc < 3)
             usageErr("%s -a file\n", argv[0]);
-        outputFd = open(argv[2], openFlags, filePerms);
+        filename = argv[2];
+    } else {
+        filename = argv[1];
+        openFlags |= O_TRUNC;
+    }
+
+    outputFd = open(filename, openFlags, filePerms);
+    if (outputFd == -1)
+        errExit("opening file %s", filename);
+
+    if (strcmp("-a", argv[1]) == 0)
         if (lseek(outputFd, 0, SEEK_END) == -1)
             errExit("lseek");
-    } else {
-        outputFd = open(argv[1], openFlags, filePerms);
+    
+    while ((numRead = read(0, buf, BUF_SIZE)) > 0 ) {
+        if (write(1, buf, numRead) != numRead)
+            fatal("couldn't write whole buffer to stdout");
+
+        if (write(outputFd, buf, numRead) != numRead)
+            fatal("couldn't write whole buffer to file");
     }
-    if (outputFd == -1)
-        errExit("opening file %s", argv[1]);
 
-    numRead = read(0, buf, BUF_SIZE - 1);
-    buf[numRead] = '\0';
-    write(1, buf, numRead);
-    if (write(outputFd, buf, numRead) != numRead)
-        fatal("couldn't write whole buffer");
-        
+    if (numRead == -1)
+        errExit("read");
 
+    if (close(outputFd) == -1)
+        errExit("close");
 }
